299ad0b93533767838899459c8d6ceac2a74a734
lgarithm <lgarithm@gmail.com>
2017-11-08 23:58:59 +0800

    use maybe monad

--- Kumo/Actor/Remote.hs
+++ Kumo/Actor/Remote.hs
@@ -7,10 +7,12 @@ module Kumo.Actor.Remote where
 import qualified Data.ByteString.Char8 as C8 (pack, unpack)
 import qualified Data.CaseInsensitive  as CI (mk)
 import           Data.List.Split       (splitOn)
+import           Data.Maybe            (fromMaybe)
 import           Kumo.Actor.Pid        (Pid (Nobody),
                                         Serializable (deserialize), hdrSender,
                                         remote, send)
 import           Network.HTTP.Types    (status200)
+import           Network.URI           (parseURI)
 import           Network.Wai           (Request, Response, rawPathInfo,
                                         requestBody, requestHeaders,
                                         requestMethod, responseLBS)
@@ -33,12 +35,14 @@ host pid req respond =
                 _        -> print "deserialize failed" >> respond (okWithText "deserialize failed")
         _           -> respond $ okWithText "OK"
 
-parseSender req =
-    let hdrs = requestHeaders req
-    in  case lookup (CI.mk . C8.pack $ hdrSender) hdrs of
-        Nothing       -> Nobody
-        Just str -> case words (C8.unpack str) of
-                        ["remote", endpoint] -> remote endpoint
-                        _                    -> Nobody
+parseSender :: Request -> Pid m
+parseSender req = fromMaybe Nobody $
+    lookup (CI.mk . C8.pack $ hdrSender) (requestHeaders req) >>=
+    parseRemote                                               >>=
+    parseURI                                                  >>=
+    Just . remote . show
+        where parseRemote str = case words (C8.unpack str) of
+                ["remote", endpoint] -> Just endpoint
+                _                    -> Nothing
 
 okWithText = responseLBS status200 [(CI.mk "Content-Type", "text/plain")]
